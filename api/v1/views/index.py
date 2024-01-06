7d4d045c6295416f8d34e5925dcc8919cdfa9242
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-01-06 13:23:16 +0100

    Create stats method and return json of all stats count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 App views for AirBnB_clone_v3
 """
 
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
 from api.v1.views import app_views
 
@@ -13,3 +13,22 @@ def status():
     """ returns status """
     status = {"status": "OK"}
     return jsonify(status)
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        STATS = {
+            "amenities": 47, 
+            "cities": 36, 
+            "places": 154, 
+            "reviews": 718, 
+            "states": 27, 
+            "users": 31
+        }
+        for key, value in STATS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
